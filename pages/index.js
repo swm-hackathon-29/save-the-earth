f0260f05253a54c38f5dcbceecab3ac954a35a69
Byeonggon Lee <gonny952@gmail.com>
2021-05-13 14:34:07 +0900

    Change title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          지구를 지켜라! 
         </h1>
 
         <p className={styles.description}>
