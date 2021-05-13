faec6cb49686134063c0ac75b03a81d648c505c4
Byeonggon Lee <gonny952@gmail.com>
2021-05-13 22:20:40 +0900

    Add all cities waste

--- pages/api/wastes/[city].js
+++ pages/api/wastes/[city].js
@@ -3,6 +3,7 @@ const redis = require('../../../src/clients').redis
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 const API_KEY_ENC = process.env.NEXT_PUBLIC_API_KEY_ENC
 const API_KEY_DEC = process.env.NEXT_PUBLIC_API_KEY_DEC
+const fetchCities = require('../cities').fetchCities
 
 async function fetchCityWastes(disYear, disMonth, cityCode) {
   const redisKey = 'city_wastes'
@@ -14,19 +15,24 @@ async function fetchCityWastes(disYear, disMonth, cityCode) {
 
   wastes = []
   for (let page = 1; ; ++page) {
-    const {data: {data: wastesRes}} = await axios.get(`${API_BASE_URL}/getCityDateList`, {
-      params: {
-        'ServiceKey': API_KEY_DEC,
-        'type': 'json',
-        disYear,
-        disMonth,
-        cityCode,
-        page,
-      }
-    })
-    if (wastesRes.list === null || wastesRes.list.length == 0)
-      break;
-    wastes.push(...wastesRes.list)
+    try {
+      const {data: {data: wastesRes}} = await axios.get(`${API_BASE_URL}/getCityDateList`, {
+        params: {
+          'ServiceKey': API_KEY_DEC,
+          'type': 'json',
+          disYear,
+          disMonth,
+          cityCode,
+          page,
+        }
+      })
+      if (wastesRes.list === null || wastesRes.list.length == 0)
+        break;
+      wastes.push(...wastesRes.list)
+    } catch (err) {
+      console.log(err.response.data)
+      break
+    }
   }
   wastes.sort((a, b) => b.disDate - a.disDate)
   await redis.hmsetAsync(redisKey, redisField, JSON.stringify(wastes))
@@ -34,8 +40,15 @@ async function fetchCityWastes(disYear, disMonth, cityCode) {
   return {[cityCode]: wastes}
 }
 
-async function fetchCityWasteOfMonth(disYear, disMonth, cityCode, aptCode) {
-  const wastes = (await fetchCityWastes(disYear, disMonth, cityCode, aptCode))[cityCode]
+async function fetchAllCitiesWastes(disYear, disMonth) {
+  return Promise.all(
+    Object.keys(await fetchCities())
+    .flatMap(async (cityCode) => (await fetchCityWastes(disYear, disMonth, cityCode)))
+  )
+}
+
+async function fetchCityWasteOfMonth(disYear, disMonth, cityCode) {
+  const wastes = (await fetchCityWastes(disYear, disMonth, cityCode))[cityCode]
 
   return {
     [cityCode]: wastes.slice(1).reduce((a, b, idx, wastes) => ({
@@ -55,8 +68,13 @@ export default async (req, res) => {
   const month = String(req.query.month || date.getMonth() + 1).padStart(2, '0')
   const total = Boolean(req.query.total || false)
   const city = req.query.city
-  const wastes = total ? (await fetchCityWasteOfMonth(year, month, city))
-    : (await fetchCityWastes(year, month, city))
-
-  return res.status(200).json(wastes)
+  if (city === 'all')
+    return res.status(200).json(
+      await fetchAllCitiesWastes(year, month)
+    )
+  else
+    return res.status(200).json(
+      total ? (await fetchCityWasteOfMonth(year, month, city))
+      : (await fetchCityWastes(year, month, city))
+    )
 }
